62f05c6223eb143768eb7a119f43a8ceba237c52
Alexei Lourie <alexeilourie@gmail.com>
2018-10-30 00:59:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,10 @@ def summon_captain_planet(array)
   array.map! { |element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.select do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
