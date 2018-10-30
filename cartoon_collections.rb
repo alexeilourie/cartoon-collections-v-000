bb911532be566ac47e7b7256a8bbc5564752ba4e
Alexei Lourie <alexeilourie@gmail.com>
2018-10-30 01:03:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,13 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.select do |call|
-    if call.length > 4
-      return false
-    else
-      return true
-    end
-  end
+  array.any? {|i| i.length > 4}
 end
 
 def find_the_cheese# code an argument here
